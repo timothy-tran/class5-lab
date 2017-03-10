3ad9794f14a6b37693c933ea23a08575eeab031e
Timothy Tran <timotran@gmail.com>
2017-03-10 15:51:05 -0800

    Add problem 1 solution

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a,b){ //eslint-disable-line
-
+  var c = a + b;
+  var sumArray = [c, 'The sum of ' + a + ' and ' + b + ' is ' + c];
+  return sumArray;
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+//testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -27,7 +29,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
-
+  var c = a * b;
+  return [c, ];
 }
 
 // Here is the test for multiply(); uncomment it to run it
